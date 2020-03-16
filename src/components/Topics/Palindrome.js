cbe8a7dc0e7b177a68735ae4bf39f36bfe58143c
Jason Boyer <jboyer@Jasons-MacBook-Pro.local>
2020-03-15 17:40:40 -0700

    14Mar

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,31 +1,43 @@
 import React, { Component } from 'react';
 
-export default class Palindrome extends Component {
-   
-    constructor () {
-        super();
-    
-        this.state ={
-            unserInput: '',
-            palindrome: ''
-    
-        };
-    } 
-handleChange(val) {
-    this.setState({userInput: val })
-}
+export default class FilterString extends Component {
 
-render() {
+  constructor() {
+    super();
+
+    this.state = {
+      names: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy'],
+      userInput: '',
+      filteredNames: []
+    };
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+  filterNames(userInput) {
+    let names = this.state.names;
+    let filteredNames = [];
+
+    for ( let i = 0; i < names.length; i++ ) {
+      if ( names[i].includes(userInput) ) {
+        filteredNames.push(names[i]);
+      }
+    }
+
+    this.setState({ filteredNames: filteredNames });
+  }
+
+  render() {
     return (
-        <div className ="puzzleBox palindromePB">
-        <h4> Palindrome </h4>
-        <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value)}/>
-        <button className="confirmationButton" onClick={ () =>
-        this.isPalindrome(this.state.unserInput)}> Check </button>
-        <span className='resultsBox'> Palindrome: {this.state.palindrome}</span>
-        </div>
+      <div className="puzzleBox filterStringPB">
+        <h4> Filter String </h4>
+        <span className="puzzleText"> Names: { JSON.stringify(this.state.names, null, 10) } </span>
+        <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }/>
+        <button className="confirmationButton" onClick={ () => this.filterNames(this.state.userInput) }> Filter </button>
+        <span className="resultsBox filterStringRB"> Filtered Names: { JSON.stringify(this.state.filteredNames, null, 10) } </span>
+      </div>
     )
+  }
 }
-}
-
-
